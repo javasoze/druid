93efd774f489bf5d7730f0812061b9ce66ec86a4
Gian Merlino <gianmerlino@gmail.com>
2016-01-09 11:41:47 -0800

    DataSource metadata.
    Geared towards supporting transactional inserts of new segments. This involves an
    interface "DataSourceMetadata" that allows combining of partially specified metadata
    (useful for partitioned ingestion).
    
    DataSource metadata is stored in a new "dataSource" table.

--- indexing-service/src/main/java/io/druid/indexing/common/task/IndexTask.java
+++ indexing-service/src/main/java/io/druid/indexing/common/task/IndexTask.java
@@ -227,7 +227,7 @@ public class IndexTask extends AbstractFixedIntervalTask
         segments.add(segment);
       }
     }
-    toolbox.pushSegments(segments);
+    toolbox.publishSegments(segments);
     return TaskStatus.success(getId());
   }
 
