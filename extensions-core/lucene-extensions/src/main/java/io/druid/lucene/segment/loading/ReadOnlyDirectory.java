9a48d3d704f26d13733a141435aa625b1f994fba
yaochitc <yaochitc@gmail.com>
2016-05-31 22:26:50 +0800

    add lucene extension

---
+++ extensions-core/lucene-extensions/src/main/java/io/druid/lucene/segment/loading/ReadOnlyDirectory.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to Metamarkets Group Inc. (Metamarkets) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Metamarkets licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package io.druid.lucene.segment.loading;
+
+import io.druid.lucene.LuceneDirectory;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ */
+public class ReadOnlyDirectory implements LuceneDirectory {
+    private final IndexReader indexReader;
+
+    public ReadOnlyDirectory(File parentDir) throws IOException {
+        FSDirectory luceneDir = FSDirectory.open(parentDir.toPath());
+        indexReader = DirectoryReader.open(luceneDir);
+    }
+
+    @Override
+    public int numRows() {
+        return indexReader.numDocs();
+    }
+
+    @Override
+    public IndexReader getIndexReader() {
+        return indexReader;
+    }
+
+    @Override
+    public void close() throws IOException {
+        indexReader.close();
+    }
+}
